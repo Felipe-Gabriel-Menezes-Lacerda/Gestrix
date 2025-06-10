2367cd67143e46ba884cce564666c83656fbaf5a
Felipe Gabriel Menezes Lacerda <felipegabrielmenezeslacerda@gmail.com>
2025-06-09 21:30:56 -0300

    Adicionando módulos de produtos na camada model,  sessão de produtos para os produtos na camada repositories

--- Gestrix/Gestrix/Project/Repositories/FirebaseProductSection/FirebaseSetProducts.cs
+++ Gestrix/Gestrix/Project/Repositories/FirebaseProductSection/FirebaseSetProducts.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Gestrix.Project.Repositories
+namespace Gestrix.Project.Repositories.FirebaseProductSection
 {
     class FirebaseSetProducts
     {
